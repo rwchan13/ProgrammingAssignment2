8695e35b5ad9991427e0f62e4432c05cc6d7ab6e
Raymond Chan <raymond.chan.wai.mun@gmail.com>
2014-09-20 22:48:07 +0800

    Matrix inversions with caching.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Takes a non singular matrix and inverts and caches the results.
 
-## Write a short comment describing this function
+## Creates a matrix data structure that can cache a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+##Returns the inverse of the input matrix. 
+##Cached results are returned if available 
+##otherwise a new solve is done.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
